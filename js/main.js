ebe1cdcd5e664b4303d1a356f6418e39a880be56
Tate <tschron@my.gcu.edu>
2023-03-05 20:22:35 -0700

    loading changes

--- js/main.js
+++ js/main.js
@@ -2,4 +2,5 @@ import loadComponents from './loader.js'
 
 window.onload = () => {
     loadComponents('#header', '/components/header.html')
+    loadComponents('#footer', '/components/footer.html')
 }
