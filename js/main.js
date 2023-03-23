0a0df24f82507fd89c557191f36ee53c51102aac
Tate <tschron@my.gcu.edu>
2023-03-22 21:28:05 -0700

    misc

--- js/main.js
+++
@@ -1,6 +0,0 @@
-import loadComponents from '/Shell-Shield/js/loader.js'
-
-window.onload = () => {
-    loadComponents('#header', '/Shell-Shield/components/header.html')
-    loadComponents('#footer', '/Shell-Shield/components/footer.html')
-}
