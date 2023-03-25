fd0622b0a9ee406e8969f3a22773d277f43375b1
Tate <tschron@my.gcu.edu>
2023-03-24 22:36:37 -0700

    mobile nav implementation

--- js/main.js
+++ js/main.js
@@ -1,15 +1,23 @@
 const navOpen = document.querySelector('#nav-open')
 const navClose = document.querySelector('#nav-close')
 const mobileNav = document.querySelector('#mobile-nav')
+const mobileNavPanel = mobileNav.querySelector('.panel')
 
-navOpen.addEventListener('click', () => {
-    mobileNav.style.display = 'block'
-    document.body.style.overflow = 'hidden'
-})
+document.addEventListener('click', (event) => {
+    if (event.target == navOpen) {
+        mobileNav.style.display = 'block'
+        document.body.style.overflow = 'hidden'
+    }
 
-navClose.addEventListener('click', () => {
-    mobileNav.style.display = 'none'
-    document.body.style.overflow = 'scroll'
+    if (event.target == navClose) {
+        mobileNav.style.display = 'none'
+        document.body.style.overflow = 'scroll'
+    }
+
+    if (event.target == mobileNav && event.target != mobileNavPanel) {
+        mobileNav.style.display = 'none'
+        document.body.style.overflow = 'scroll'
+    }
 })
 
 window.addEventListener('resize', () => {
