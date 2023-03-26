e86ff8bbd52171d55070d8d2f90b65981d484aed
Tate <tschron@my.gcu.edu>
2023-03-26 01:22:26 -0700

    cleanup

--- php/emailHandling.php
+++
@@ -1,7 +0,0 @@
-<?php
-$recipient = "shellshieldsolutions@gmail.com";
-$sender = $_POST['sender'];
-$address = $_POST['address'];
-$message = $_POST['message'];
-mail($recipient, "Message from $sender via Shell Shield Solutions website", $message)
-?>
